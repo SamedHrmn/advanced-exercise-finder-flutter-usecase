4e15ec2c263b17f17cdd176ac783058eebca7bb9
SamedHrmn <samed_harman@hotmail.com>
2024-03-10 18:12:31 +0300

    slidable pane radius fixed

--- lib/features/program/widgets/program_exercise_card.dart
+++ lib/features/program/widgets/program_exercise_card.dart
@@ -12,58 +12,61 @@ class ProgramExerciseCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Slidable(
-      endActionPane: ActionPane(
-        motion: const ScrollMotion(),
-        children: [
-          SlidableAction(
-            icon: Icons.delete,
-            backgroundColor: Colors.red,
-            onPressed: (_) {
-              programViewModel.deleteExercise(exercise);
-            },
-          ),
-        ],
-      ),
-      child: Container(
-        height: 150,
-        margin: const EdgeInsets.only(bottom: 16),
-        padding: const EdgeInsets.all(16),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          boxShadow: [
-            BoxShadow(color: Colors.black.withOpacity(0.2), spreadRadius: 2, blurRadius: 4),
-          ],
-          color: Colors.white,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 16),
+      child: Slidable(
+        endActionPane: ActionPane(
+          motion: const BehindMotion(),
           children: [
-            Row(
-              children: [
-                AppText(text: exercise.name),
-                const Spacer(),
-                IconButton.filled(
-                  style: IconButton.styleFrom(
-                    fixedSize: const Size(24, 24),
-                    minimumSize: Size.zero,
-                    iconSize: 16,
-                    padding: EdgeInsets.zero,
-                    tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                  ),
-                  onPressed: null,
-                  icon: const Icon(Icons.add),
-                ),
-              ],
+            SlidableAction(
+              icon: Icons.delete,
+              borderRadius: BorderRadius.circular(16),
+              backgroundColor: Colors.red,
+              onPressed: (_) {
+                programViewModel.deleteExercise(exercise);
+              },
             ),
-            Container(
-              decoration: BoxDecoration(borderRadius: BorderRadius.circular(16), color: Colors.grey.shade300),
-              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-              child: AppText(text: exercise.type),
-            ),
-            AppText(text: exercise.muscle),
           ],
+        ),
+        child: Container(
+          height: 150,
+          padding: const EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            boxShadow: [
+              BoxShadow(color: Colors.black.withOpacity(0.2), spreadRadius: 2, blurRadius: 4),
+            ],
+            color: Colors.white,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  AppText(text: exercise.name),
+                  const Spacer(),
+                  IconButton.filled(
+                    style: IconButton.styleFrom(
+                      fixedSize: const Size(24, 24),
+                      minimumSize: Size.zero,
+                      iconSize: 16,
+                      padding: EdgeInsets.zero,
+                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    ),
+                    onPressed: null,
+                    icon: const Icon(Icons.add),
+                  ),
+                ],
+              ),
+              Container(
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(16), color: Colors.grey.shade300),
+                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                child: AppText(text: exercise.type),
+              ),
+              AppText(text: exercise.muscle),
+            ],
+          ),
         ),
       ),
     );
